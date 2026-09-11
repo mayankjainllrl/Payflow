693671b08375d962f2cce59cf469ad8d3a81ffff
Mayank Jain <mayankjainllrl@gmail.com>
2026-09-11 07:31:02 +0530

    Fix database URL driver (asyncpg)

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "Payflow"
     environment: str = "dev"
-    database_url: str = "postgresql+sqlalchemy://payflow:payflow@db:5432/payflow"
+    database_url: str = "postgresql+asyncpg://payflow:payflow@db:5432/payflow"
     
 settings = Settings()
